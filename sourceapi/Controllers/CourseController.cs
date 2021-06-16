8b997451985a3756336dbe8d4fec65f70840afb4
yogeshwari-gowda <yogeshwari-gowda@INMBIEL8HPYMW2>
2021-06-16 15:51:36 +0530

    Add project files.

---
+++ sourceapi/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sourceapi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sourceapi.Controllers
+{
+    [ApiController]
+    [Route("/api/Course")]
+    public class CourseController : ControllerBase
+    {
+        private CourseService _course_service;
+
+        public CourseController(CourseService _svc)
+        {
+            _course_service = _svc;
+        }
+
+        [HttpGet]
+        public IActionResult GetCourses()
+        {
+            return Ok(_course_service.GetCourses());
+        }
+    }
+}
---
+++ sourceapi/Services/CourseService.cs
@@ -0,0 +1,28 @@
+﻿using sourceapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sourceapi.Services
+{
+    public class CourseService
+    {
+        public List<Course> listCourse;
+
+        public CourseService()
+        {
+            listCourse = new List<Course>()
+            {
+                new Course(){CourseID=1,CourseName="AZ-204"},
+                new Course(){CourseID=2,CourseName="AZ-303"},
+                new Course(){CourseID=3,CourseName="AZ-304"}
+            };
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return (listCourse);
+        }
+    }
+}
